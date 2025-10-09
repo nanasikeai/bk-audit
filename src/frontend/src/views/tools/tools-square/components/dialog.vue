1b0887931b4c04bd12e99c4304e9580f7f1b5d1e
nanasikeai <nanasikeai@gmail.com>
2025-10-09 16:45:26 +0800

    feat: feat: 字段下钻支持关联多个工具 --story=126594081  工具使用优化
    1. 工具下钻显示使用下钻显示名称
    2. tips优化

--- src/frontend/src/views/tools/tools-square/components/dialog.vue
+++ src/frontend/src/views/tools/tools-square/components/dialog.vue
@@ -299,7 +299,8 @@
         target_value_type: string;
         target_value: string;
         target_field_type?: string;
-      }>
+      }>;
+      drill_name?: string;
     }>;
     enum_mappings: {
       collection_id: string;
@@ -599,6 +600,7 @@
     searchFormData.value = {};
     // 创建table
     columns.value = data.config.output_fields.map((item) => {
+      console.log(item, 'item');
       const renderCell = ({ data }: { data: Record<any, any> }) => {
         const rawVal = data[item.raw_name];
         // 如果有enum映射，优先用映射的name
@@ -607,7 +609,7 @@
           ? mappings.find((m: any) => String(m.key) === String(rawVal))
           : undefined;
         const display = mapped ? mapped.name : rawVal;
-        if (item.drill_config === null || item.drill_config.length === 0) {
+        if (item.drill_config === null || (item.drill_config.length === 1 && !item.drill_config[0].tool.uid)) {
           // 普通单元格
           return <span
             v-bk-tooltips={{
@@ -617,6 +619,9 @@
               }),
               disabled: !mapped,
             }}
+            style={{
+              cursor: 'pointer',
+            }}
             class={{ tips: mapped }}
           >
             {display}
@@ -625,23 +630,52 @@
         // 可下钻的列，显示按钮
         return (
           <div>
-            <bk-button
-              v-bk-tooltips={{
-                content: t('映射对象', {
-                  key: mapped?.key,
-                  name: mapped?.name,
-                }),
-                disabled: !mapped,
-              }}
-              class={{ tips: mapped }}
-              theme="primary"
-              text
-              onClick={(e: any) => {
-                e.stopPropagation(); // 阻止事件冒泡
-                handleFieldDownClick(item, data);
+            <bk-popover
+              placement="top"
+              theme="black"
+              v-slots={{
+                content: () => (
+                  <>
+                    {
+                      mapped && (
+                        <>
+                          <span>
+                          { t('存储值: ') }
+                          </span>
+                          <span>
+                            { mapped?.key }
+                          </span>
+                          <br />
+                          <span>
+                            { t('展示文本: ') }
+                          </span>
+                          <span>
+                            { mapped?.name }
+                          </span>
+                        </>
+                      )
+                    }
+                    <div style={{
+                      marginTop: '8px',
+                    }}>
+                      { t('点击查看此字段的证据下探') }
+                    </div>
+                  </>
+                ),
               }}>
-              {display}
-            </bk-button>
+              <span
+                style={{
+                  cursor: 'pointer',
+                  color: '#3a84ff',
+                }}
+                class={{ tips: mapped }}
+                onClick={(e: any) => {
+                  e.stopPropagation(); // 阻止事件冒泡
+                  handleFieldDownClick(item, data);
+                }}>
+                {display}
+              </span>
+            </bk-popover>
             <bk-popover
               placement="top"
               theme="black"
@@ -650,7 +684,7 @@
                   <div>
                     {item.drill_config.map(config => (
                       <div key={config.tool.uid}>
-                        • {getToolNameAndType(config.tool.uid).name}
+                        {config.drill_name || getToolNameAndType(config.tool.uid).name}
                         <bk-button
                           class="ml8"
                           theme="primary"
@@ -659,7 +693,10 @@
                             e.stopPropagation(); // 阻止事件冒泡
                             handleFieldDownClick(item, data, config.tool.uid);
                           }}>
-                          {t('查看')}
+                          {t('去查看')}
+                          <audit-icon
+                            class="mr-18"
+                            type="jump-link" />
                         </bk-button>
                       </div>
                     ))}
@@ -1401,5 +1438,12 @@
 
 :deep(.bk-table .bk-table-head .col-resize-drag) {
   background-color: #fafbfd;
+}
+</style>
+<style lang="postcss">
+.tools-use-dialog {
+  .bk-modal-body {
+    border-radius: 16px 16px 8px 8px;
+  }
 }
 </style>
